eb4e4707c42eba543beedd84ee8b27c1fabe7528
bhlp0151 <sarasani.vishalreddy@equidhi.org>
2025-07-08 11:22:26 +0530

    kebbi package changes

--- apps/health_campaign_field_worker_app/android/app/src/main/kotlin/org/egov/kebbi/MainActivity.kt
+++ apps/health_campaign_field_worker_app/android/app/src/main/kotlin/org/egov/kebbi/MainActivity.kt
@@ -1,4 +1,4 @@
-package org.egov.oyo
+package org.egov.kebbi
 
 import android.content.BroadcastReceiver
 import android.content.Context
--- apps/health_campaign_field_worker_app/android/app/src/main/kotlin/org/egov/kebbi/location_service.kt
+++ apps/health_campaign_field_worker_app/android/app/src/main/kotlin/org/egov/kebbi/location_service.kt
@@ -1,4 +1,4 @@
-package org.egov.oyo
+package org.egov.kebbi
 
 import android.annotation.TargetApi
 import android.app.NotificationChannel
