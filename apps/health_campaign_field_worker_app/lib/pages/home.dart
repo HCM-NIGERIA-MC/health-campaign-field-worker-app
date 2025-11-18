4b15a9a7b14288fe2ce16a64297ccfca9bdbeb64
psbskb <susanta.behera@equidhi.org>
2025-11-18 17:26:50 +0530

    rearrange the beneficiaryLabel to the first

--- apps/health_campaign_field_worker_app/lib/pages/home.dart
+++ apps/health_campaign_field_worker_app/lib/pages/home.dart
@@ -837,18 +837,19 @@ class _HomePageState extends LocalizedState<HomePage> {
     };
 
     final homeItemsLabel = <String>[
+      // i18.home.beneficiaryReferralLabel,
+      i18.home.beneficiaryLabel,
       // INFO: Need to add items label of package Here
       i18.home.manageAttendanceLabel,
-
-      // i18.home.beneficiaryReferralLabel,
-      i18.home.mySurveyForm,
-      i18.home.beneficiaryLabel,
       // i18.home.manageStockLabel,
       // i18.home.stockReconciliationLabel,
       // i18.home.viewReportsLabel,
       i18.home.viewSummaryReportsLabel,
-      i18.home.syncDataLabel,
+      i18.home.syncDataLabel, // INFO: Need to add items label of package Here
+      i18.home.manageAttendanceLabel,
       i18.home.fileComplaint,
+      i18.home.mySurveyForm,
+
       i18.home.db,
       i18.home.dashboard,
     ];
