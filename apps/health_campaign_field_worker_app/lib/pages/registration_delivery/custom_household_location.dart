0b9812ea42c86c7b15c05fcb0d301f235beee14c
aviral-beehyv <aviral.gupta@beehyv.com>
2025-11-19 15:48:06 +0530

    removed required asterik from postal code and address line 1 from household location page during registration flow

--- apps/health_campaign_field_worker_app/lib/pages/registration_delivery/custom_household_location.dart
+++ apps/health_campaign_field_worker_app/lib/pages/registration_delivery/custom_household_location.dart
@@ -398,7 +398,7 @@ class CustomHouseholdLocationPageState
                                   .replaceAll('{}', maxLength.toString()),
                             },
                             builder: (field) => LabeledField(
-                              isRequired: true,
+                              isRequired: false,
                               label: localizations.translate(i18
                                   .householdLocation
                                   .householdAddressLine1LabelText),
@@ -482,7 +482,7 @@ class CustomHouseholdLocationPageState
                                       .replaceAll('{}', maxLength.toString()),
                                 },
                                 builder: (field) => LabeledField(
-                                    isRequired: true,
+                                    isRequired: false,
                                     label: localizations.translate(i18
                                         .householdLocation.postalCodeFormLabel),
                                     child: DigitTextFormInput(
