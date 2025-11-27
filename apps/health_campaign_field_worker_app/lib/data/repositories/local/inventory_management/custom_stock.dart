e3baf2b799fd87c5a652c16c5c3c183fc6d7fb9f
psbskb <susanta.behera@equidhi.org>
2025-11-27 16:57:04 +0530

    stock search fix

--- apps/health_campaign_field_worker_app/lib/data/repositories/local/inventory_management/custom_stock.dart
+++ apps/health_campaign_field_worker_app/lib/data/repositories/local/inventory_management/custom_stock.dart
@@ -34,8 +34,8 @@ class CustomStockLocalRepository
       var results = [];
       final selectQuery = sql.select(sql.stock).join([]);
       final now = DateTime.now();
-      final startOfDay = DateTime(now.year, now.month, now.day);
-      final endOfDay = startOfDay.add(const Duration(days: 1));
+      // final startOfDay = DateTime(now.year, now.month, now.day);
+      // final endOfDay = startOfDay.add(const Duration(days: 1));
       results = await (selectQuery
             ..where(
               buildAnd(
@@ -66,10 +66,10 @@ class CustomStockLocalRepository
                     sql.stock.transactionReason.isIn(
                       query.transactionReason!,
                     ),
-                  sql.stock.clientCreatedTime.isBiggerOrEqualValue(
-                          startOfDay.millisecondsSinceEpoch) &
-                      sql.stock.clientCreatedTime
-                          .isSmallerThanValue(endOfDay.millisecondsSinceEpoch),
+                  // sql.stock.clientCreatedTime.isBiggerOrEqualValue(
+                  //         startOfDay.millisecondsSinceEpoch) &
+                  //     sql.stock.clientCreatedTime
+                  //         .isSmallerThanValue(endOfDay.millisecondsSinceEpoch),
                 ],
               ),
             )
