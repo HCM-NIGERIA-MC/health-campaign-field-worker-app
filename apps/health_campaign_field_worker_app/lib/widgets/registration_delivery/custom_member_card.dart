417ba27b6b1f8a347cedb11d73ffae166248726d
egov-skb <susanta.behera@equidhi.org>
2025-11-28 13:52:55 +0530

    edit button fix in member card

--- apps/health_campaign_field_worker_app/lib/widgets/registration_delivery/custom_member_card.dart
+++ apps/health_campaign_field_worker_app/lib/widgets/registration_delivery/custom_member_card.dart
@@ -396,6 +396,24 @@ class CustomMemberCard extends StatelessWidget {
     );
   }
 
+  bool isCurrentCycleData(BuildContext context, List<TaskModel> task) {
+    if (task.isEmpty) return true;
+    final currentCycle = context.selectedCycle;
+    final taskCycleIndex = task.first.additionalFields?.fields
+        .firstWhereOrNull(
+          (e) =>
+              e.key ==
+              additional_fields_local.AdditionalFieldsType.cycleIndex.toValue(),
+        )
+        ?.value;
+    if (taskCycleIndex != null && currentCycle != null) {
+      if (int.tryParse(taskCycleIndex) == currentCycle.id) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -482,14 +500,15 @@ class CustomMemberCard extends StatelessWidget {
                   ),
                 ],
               ),
-              ((tasks ?? [])
-                              .where(
-                                (element) =>
-                                    element.status ==
-                                    Status.administeredSuccess.toValue(),
-                              )
-                              .lastOrNull ==
-                          null &&
+              ((!isCurrentCycleData(context, tasks ?? []) ||
+                          (tasks ?? [])
+                                  .where(
+                                    (element) =>
+                                        element.status ==
+                                        Status.administeredSuccess.toValue(),
+                                  )
+                                  .lastOrNull ==
+                              null) &&
                       !isSMCDelivered &&
                       !isBeneficiaryIneligible &&
                       !isBeneficiaryReferred)
