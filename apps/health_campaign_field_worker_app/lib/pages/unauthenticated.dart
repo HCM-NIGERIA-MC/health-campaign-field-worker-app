d4169d056c9e8cc8fa9c75e015b3d34c94263c31
psbskb <susanta.behera@equidhi.org>
2025-12-05 15:04:33 +0530

    tablet view apk ui fix

--- apps/health_campaign_field_worker_app/lib/pages/unauthenticated.dart
+++ apps/health_campaign_field_worker_app/lib/pages/unauthenticated.dart
@@ -1,10 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/tablet_to_phone_view_wrapper.dart';
+
 @RoutePage()
 class UnauthenticatedPageWrapper extends StatelessWidget {
   const UnauthenticatedPageWrapper({super.key});
 
   @override
-  Widget build(BuildContext context) => const AutoRouter();
+  Widget build(BuildContext context) =>
+      const TabletToPhoneViewWrapper(child: AutoRouter());
 }
