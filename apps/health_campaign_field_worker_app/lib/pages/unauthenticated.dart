5f40cd03143b8555a79503c52652d6cc1800a9e1
psbskb <susanta.behera@equidhi.org>
2025-12-05 15:39:53 +0530

    move the TabletToPhoneViewWrapper to  main file

--- apps/health_campaign_field_worker_app/lib/pages/unauthenticated.dart
+++ apps/health_campaign_field_worker_app/lib/pages/unauthenticated.dart
@@ -1,13 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
-import '../widgets/tablet_to_phone_view_wrapper.dart';
-
 @RoutePage()
 class UnauthenticatedPageWrapper extends StatelessWidget {
   const UnauthenticatedPageWrapper({super.key});
 
   @override
-  Widget build(BuildContext context) =>
-      const TabletToPhoneViewWrapper(child: AutoRouter());
+  Widget build(BuildContext context) => const AutoRouter();
 }
