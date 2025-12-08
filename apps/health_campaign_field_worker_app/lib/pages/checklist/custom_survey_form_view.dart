9d43375dc7216676789391e724b64363e407d3d5
egov-skb <susanta.behera@equidhi.org>
2025-12-08 21:13:37 +0530

    allowed / for the check dob regx

--- apps/health_campaign_field_worker_app/lib/pages/checklist/custom_survey_form_view.dart
+++ apps/health_campaign_field_worker_app/lib/pages/checklist/custom_survey_form_view.dart
@@ -423,7 +423,7 @@ class CustomSurveyFormViewPageState
                                           inputFormatters: [
                                             FilteringTextInputFormatter.allow(
                                                 RegExp(
-                                              "[a-zA-Z0-9]",
+                                              "[a-zA-Z0-9/]",
                                             )),
                                           ],
                                         ));
