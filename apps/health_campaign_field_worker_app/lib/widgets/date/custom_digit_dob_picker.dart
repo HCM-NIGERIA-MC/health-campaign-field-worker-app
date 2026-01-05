50595a391b7aacea2512f594ea9c7e8f2947d3ba
egov-skb <susanta.behera@equidhi.org>
2026-01-05 17:18:52 +0530

    dob issue fixed

--- apps/health_campaign_field_worker_app/lib/widgets/date/custom_digit_dob_picker.dart
+++ apps/health_campaign_field_worker_app/lib/widgets/date/custom_digit_dob_picker.dart
@@ -77,7 +77,15 @@ class CustomDigitDobPicker extends StatelessWidget {
               confirmText: confirmText,
               onChangeOfFormControl: onChangeOfFormControl,
               end: finalDate ??
-                  DateTime.now().subtract(const Duration(days: 30)),
+                  DateTime.now().subtract(Duration(
+                      days: DateTime.now().month == 2 &&
+                              DigitDateUtils.isLeapYear(DateTime.now().year)
+                          ? 29
+                          : DateTime.now().month == 2
+                              ? 28
+                              : [4, 6, 9, 11].contains(DateTime.now().month)
+                                  ? 30
+                                  : 31)),
             ),
             const SizedBox(height: 16),
             // Text widget to display a separator label between the date picker and age fields
