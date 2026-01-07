a39747125262de7ca6fad9d8c0f499ccfe8566f9
psbskb <susanta.behera@equidhi.org>
2026-01-07 16:52:26 +0530

    fix: ensure survey form validation handles null case correctly

--- apps/health_campaign_field_worker_app/lib/pages/checklist/custom_survey_form_view.dart
+++ apps/health_campaign_field_worker_app/lib/pages/checklist/custom_survey_form_view.dart
@@ -124,8 +124,9 @@ class CustomSurveyFormViewPageState
                                       ),
                                     );
                                 final isValid =
-                                    surveyFormKey.currentState?.validate();
-                                if (!isValid!) {
+                                    surveyFormKey.currentState?.validate() ??
+                                        false;
+                                if (!isValid) {
                                   return;
                                 }
 
