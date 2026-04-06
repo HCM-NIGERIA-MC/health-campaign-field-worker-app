77578e173433b1d77c425f503977e8086c904b26
psbskb22 <psb.skb22@gmail.com>
2026-04-07 04:57:29 +0530

    change the scan qr visibility for different roles

--- apps/health_campaign_field_worker_app/lib/pages/registration_delivery/custom_search_beneficiary.dart
+++ apps/health_campaign_field_worker_app/lib/pages/registration_delivery/custom_search_beneficiary.dart
@@ -1178,6 +1178,9 @@ class _CustomSearchBeneficiaryPageState
                           "DELIVERY_DETAILS_DUPLICATE_VOUCHER_SCAN_VALIDATION"
                     }
                   ];
+                  registrationSchemaData['pages']["beneficiaryDetails"]
+                          ["properties"]['tag']['hidden'] =
+                      context.isDistributor && context.isRegistrar;
                   registrationConfig = json.encode(registrationSchemaData);
                 }
                 if (deliveryConfigString != null) {
@@ -1254,7 +1257,8 @@ class _CustomSearchBeneficiaryPageState
     }
 
     // — Secondary button —
-    if (secondaryProp?.hidden != true) {
+    if (secondaryProp?.hidden != true &&
+        (!context.isRegistrar || !context.isDistributor)) {
       final order = secondaryProp?.order ?? 1;
       entries.add(MapEntry(
         order,
